1c6140a3d08c3bfe6e0a7f9e42d34185d1c062a1
daniel ra <winra001@hotmail.com>
2017-02-26 22:41:22 +1100

    Init

---
+++ src/main/java/com/eventra/EventraApplication.java
@@ -0,0 +1,12 @@
+package com.eventra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventraApplication.class, args);
+	}
+}
